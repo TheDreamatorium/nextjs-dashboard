43fb650fa6054282596f32102852d4e890b3bfdd
david <you@example.com>
2024-05-07 14:01:32 +0400

    chapter 13 - error handling

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
  
 export default async function Page({ params }: { params: { id: string } }) {
     const id = params.id;
@@ -9,19 +10,23 @@ export default async function Page({ params }: { params: { id: string } }) {
         fetchCustomers(),
     ]);
 
+    if (!invoice) {
+        notFound();
+      }
+
     return (
         <main>
-        <Breadcrumbs
-            breadcrumbs={[
-            { label: 'Invoices', href: '/dashboard/invoices' },
-            {
-                label: 'Edit Invoice',
-                href: `/dashboard/invoices/${id}/edit`,
-                active: true,
-            },
-            ]}
-        />
-        <Form invoice={invoice} customers={customers} />
+            <Breadcrumbs
+                breadcrumbs={[
+                { label: 'Invoices', href: '/dashboard/invoices' },
+                {
+                    label: 'Edit Invoice',
+                    href: `/dashboard/invoices/${id}/edit`,
+                    active: true,
+                },
+                ]}
+            />
+            <Form invoice={invoice} customers={customers} />
         </main>
     );
 }
